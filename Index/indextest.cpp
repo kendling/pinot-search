210bda51cde5c0b18c15e2576e97fc2ecf9af2c0
fabricecolin <fabricecolin@163c4839-fc77-d6c7-dbea-7b133176f809>
2006-07-14 15:57:09 +0000

    Updated this, it should prove useful later.

--- Index/indextest.cpp
+++ Index/indextest.cpp
@@ -14,109 +14,198 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
  
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
-#include <set>
+#include <getopt.h>
+#include <cstdlib>
+#include <cstdio>
 #include <iostream>
+#include <string>
+#include <fstream>
 
-#include "Document.h"
-#include "TokenizerFactory.h"
+#include "MIMEScanner.h"
+#include "Url.h"
 #include "XapianDatabaseFactory.h"
+#include "TokenizerFactory.h"
+#include "DownloaderFactory.h"
 #include "XapianIndex.h"
+#include "config.h"
 
 using namespace std;
 
+static struct option g_longOptions[] = {
+	{"check", 1, 0, 'c'},
+	{"help", 0, 0, 'h'},
+	{"index", 1, 0, 'i'},
+	{"stats", 0, 0, 's'},
+	{"version", 0, 0, 'v'},
+	{0, 0, 0, 0}
+};
+
 int main(int argc, char **argv)
 {
-	bool success = false;
+	string type, option;
+	int longOptionIndex = 0;
+	string parameter;
+	bool checkDocument = false, indexDocument = false, statIndex = false, success = false;
+
+	// Look at the options
+	int optionChar = getopt_long(argc, argv, "hv", g_longOptions, &longOptionIndex);
+	while (optionChar != -1)
+	{
+		set<string> engines;
+
+		switch (optionChar)
+		{
+			case 'c':
+				if (optarg != NULL)
+				{
+					parameter = optarg;
+				}
+				checkDocument = true;
+				break;
+			case 'h':
+				// Help
+				cout << "pinot-index - Query search engines from the command-line\n\n"
+					<< "Usage: pinot-search [OPTIONS] INDEXTYPE INDEXLOCATION\n\n"
+					<< "Options:\n"
+					<< "  -c, --check		check whether the given URL is in the index\n"
+					<< "  -h, --help		display this help and exit\n"
+					<< "  -i, --index		index the given URL\n"
+					<< "  -s, --stats		print statistics about the index (default option)\n"
+					<< "  -v, --version		output version information and exit\n\n";
+				cout << "\n\nExamples:\n"
+					<< "pinot-index --check http://pinot.berlios.de/ xapian ~/.pinot/index\n\n"
+					<< "pinot-index --stats xapian ~/.pinot/index\n\n"
+					<< "pinot-index --index http://pinot.berlios.de/ xapian ~/.pinot/index\n\n"
+					<< "Report bugs to " << PACKAGE_BUGREPORT << endl;
+				return EXIT_SUCCESS;
+			case 'i':
+				if (optarg != NULL)
+				{
+					parameter = optarg;
+				}
+				indexDocument = true;
+				break;
+			case 's':
+				if (optarg != NULL)
+				{
+					parameter = optarg;
+				}
+				statIndex = true;
+				break;
+			case 'v':
+				cout << "pinot-index - " << PACKAGE_STRING << "\n\n"
+					<< "This is free software.  You may redistribute copies of it under the terms of\n"
+					<< "the GNU General Public License <http://www.gnu.org/licenses/gpl.html>.\n"
+					<< "There is NO WARRANTY, to the extent permitted by law." << endl;
+				return EXIT_SUCCESS;
+			default:
+				return EXIT_FAILURE;
+		}
+
+		// Next option
+		optionChar = getopt_long(argc, argv, "hv", g_longOptions, &longOptionIndex);
+	}
 
 	if (argc < 3)
 	{
-		cerr << "Usage: " << argv[0] << " <index type> <database name> CHECK|CREATE|HASURL=<url>|INDEX=<file name>" << endl;
+		cerr << "Not enough parameters" << endl;
 		return EXIT_FAILURE;
 	}
 
-	// Check database ?
-	if (strncasecmp(argv[3], "CHECK", 5) == 0)
-	{
-		XapianIndex index(argv[2]);
+	MIMEScanner::initialize();
+	DownloaderInterface::initialize();
 
+	// FIXME: do stuff
+	XapianIndex index(argv[2]);
+
+	if (checkDocument == true)
+	{
 		if (index.isGood() == true)
 		{
-			cout << "Index has " << index.getDocumentsCount() << " document(s)" << endl;
-			success = true;
-		}
-	}
-	// Create database ?
-	else if (strncasecmp(argv[3], "CREATE", 6) == 0)
-	{
-		if (XapianDatabaseFactory::getDatabase(argv[2], false) != NULL)
-		{
-			success = true;
-		}
-	}
-	// Look for an URL ?
-	else if (strncasecmp(argv[3], "HASURL", 6) == 0)
-	{
-		XapianIndex index(argv[2]);
-
-		if ((index.isGood() == true) &&
-			(index.hasDocument(argv[3] + 7) > 0))
-		{
-			success = true;
-		}
-	}
-	// Index a file ?
-	else if (strncasecmp(argv[3], "INDEX", 5) == 0)
-	{
-		struct stat fileStat;
-		string fileName(argv[3] + 6);
-
-		if ((stat(fileName.c_str(), &fileStat) == 0) &&
-			(S_ISREG(fileStat.st_mode)))
-		{
-			char *buffer = new char[fileStat.st_size + 1];
-			int fd = open(fileName.c_str(), O_RDONLY);
-			// Read the file
-			ssize_t readBytes = read(fd, buffer, fileStat.st_size);
-			if (readBytes > 0)
+			unsigned int docId = index.hasDocument(parameter);
+			if (docId > 0)
 			{
-				// Assume file is HTML...
-				Document doc(fileName, fileName, "text/html", "");
-				doc.setData(buffer, readBytes);
-				if (doc.isBinary() == false)
+				cout << parameter << ": document ID " << docId << endl;
+				success = true;
+			}
+		}
+	}
+	if (indexDocument == true)
+	{
+		Url thisUrl(parameter);
+
+		// Which Downloader ?
+		DownloaderInterface *pDownloader = DownloaderFactory::getDownloader(thisUrl.getProtocol());
+		if (pDownloader == NULL)
+		{
+			cerr << "Couldn't obtain downloader for protocol " << thisUrl.getProtocol() << endl;
+
+			XapianDatabaseFactory::closeAll();
+			DownloaderInterface::shutdown();
+			MIMEScanner::shutdown();
+
+			return EXIT_FAILURE;
+		}
+
+		DocumentInfo docInfo(parameter, parameter, MIMEScanner::scanUrl(thisUrl), "");
+		Document *pDoc = pDownloader->retrieveUrl(docInfo);
+		if (pDoc == NULL)
+		{
+			cerr << "Download operation failed !" << endl;
+		}
+		else
+		{
+			// Tokenize this document
+			Tokenizer *pTokens = TokenizerFactory::getTokenizerByType(docInfo.getType(), pDoc);
+			if (pTokens != NULL)
+			{
+				set<string> labels;
+
+				index.setStemmingMode(IndexInterface::STORE_BOTH);
+
+				// Update an existing document or add to the index ?
+				unsigned int docId = index.hasDocument(parameter);
+				if (docId > 0)
 				{
-					set<string> labels;
-					unsigned int docId = 0;
-
-					Tokenizer *pTokens = TokenizerFactory::getTokenizer(fileName, &doc);
-					if (pTokens != NULL)
+					// Update the document
+					if (index.updateDocument(docId, *pTokens) == true)
 					{
-						// Ignore index type, use a XapianIndex
-						XapianIndex index(argv[2]);
-						index.setStemmingMode(IndexInterface::STORE_BOTH);
-						if (index.indexDocument(*pTokens, labels, docId) == true)
-						{
-							cout << "Added " << fileName << " to index, document" << docId << endl;
-							success = true;
-						}
-
-						delete pTokens;
+						success = true;
 					}
 				}
+				else
+				{
+					// Index the document
+					success = index.indexDocument(*pTokens, labels, docId);
+				}
 
-				delete[] buffer;
+				if (success == true)
+				{
+					// Flush the index
+					index.flush();
+				}
+
+				delete pTokens;
 			}
+
+			delete pDoc;
+		}
+
+		delete pDownloader;
+	}
+	if (statIndex == true)
+	{
+		if (index.isGood() == true)
+		{
+			success = true;
 		}
 	}
 
 	XapianDatabaseFactory::closeAll();
+	DownloaderInterface::shutdown();
+	MIMEScanner::shutdown();
 
+	// Did whatever operation we carried out succeed ?
 	if (success == true)
 	{
 		return EXIT_SUCCESS;
