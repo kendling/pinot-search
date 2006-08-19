cd64835bf6325e2d3aba9ba4a5288905144503a1
fabricecolin <fabricecolin@163c4839-fc77-d6c7-dbea-7b133176f809>
2006-08-19 05:24:48 +0000

    An implementation of WritableIndexInterface that talks to the daemon via DBus. Methods (un)indexDocument(s) don't do anything since they are not exported by the daemon.

---
+++ Index/DBusXapianIndex.cpp
@@ -0,0 +1,449 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <iostream>
+extern "C"
+{
+#define DBUS_API_SUBJECT_TO_CHANGE
+#include <dbus/dbus.h>
+#include <dbus/dbus-glib.h>
+#include <dbus/dbus-glib-lowlevel.h>
+}
+
+#include "Languages.h"
+#include "XapianDatabaseFactory.h"
+#include "DBusXapianIndex.h"
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::set;
+using std::min;
+
+static DBusGConnection *getBusConnection(void)
+{
+	GError *pError = NULL;
+	DBusGConnection *pBus = NULL;
+
+	pBus = dbus_g_bus_get(DBUS_BUS_SESSION, &pError);
+	if (pBus == NULL)
+	{
+		if (pError != NULL)
+		{
+			cerr << "DBusXapianIndex: couldn't connect to session bus: " << pError->message << endl;
+			g_error_free(pError);
+		}
+	}
+
+	return pBus;
+}
+
+static DBusGProxy *getBusProxy(DBusGConnection *pBus)
+{
+	if (pBus == NULL)
+	{
+		return NULL;
+	}
+
+	return dbus_g_proxy_new_for_name(pBus,
+		"de.berlios.Pinot", "/de/berlios/Pinot", "de.berlios.Pinot");
+}
+
+DBusXapianIndex::DBusXapianIndex(const string &indexName) :
+	WritableIndexInterface(),
+	XapianIndex(indexName)
+{
+}
+
+DBusXapianIndex::~DBusXapianIndex()
+{
+}
+
+void DBusXapianIndex::reopen(void)
+{
+	XapianDatabase *pDatabase = XapianDatabaseFactory::getDatabase(m_databaseName);
+	if (pDatabase != NULL)
+	{
+		Xapian::Database *pIndex = pDatabase->readLock();
+
+		// Re-open the database to the latest available version
+		pIndex->reopen();
+
+		pDatabase->unlock();
+	}
+
+}
+
+//
+// Implementation of IndexInterface
+//
+
+/// Returns false if the index couldn't be opened.
+bool DBusXapianIndex::isGood(void) const
+{
+	return m_goodIndex;
+}
+
+/// Gets the index location.
+string DBusXapianIndex::getLocation(void) const
+{
+	return m_databaseName;
+}
+
+/// Returns a document's properties.
+bool DBusXapianIndex::getDocumentInfo(unsigned int docId, DocumentInfo &docInfo) const
+{
+	return XapianIndex::getDocumentInfo(docId, docInfo);
+}
+
+/// Determines whether a document has a label.
+bool DBusXapianIndex::hasLabel(unsigned int docId, const string &name) const
+{
+	return XapianIndex::hasLabel(docId, name);
+}
+
+/// Returns a document's labels.
+bool DBusXapianIndex::getDocumentLabels(unsigned int docId, set<string> &labels) const
+{
+	return XapianIndex::getDocumentLabels(docId, labels);
+}
+
+/// Checks whether the given URL is in the index.
+unsigned int DBusXapianIndex::hasDocument(const string &url) const
+{
+	return XapianIndex::hasDocument(url);
+}
+
+/// Gets terms with the same root.
+unsigned int DBusXapianIndex::getCloseTerms(const string &term, set<string> &suggestions)
+{
+	return XapianIndex::getCloseTerms(term, suggestions);
+}
+
+/// Returns the ID of the last document.
+unsigned int DBusXapianIndex::getLastDocumentID(void) const
+{
+	return XapianIndex::getLastDocumentID();
+}
+
+/// Returns the number of documents.
+unsigned int DBusXapianIndex::getDocumentsCount(const string &labelName) const
+{
+	return XapianIndex::getDocumentsCount(labelName);
+}
+
+/// Lists document IDs.
+unsigned int DBusXapianIndex::listDocuments(set<unsigned int> &docIds,
+	unsigned int maxDocsCount, unsigned int startDoc) const
+{
+	return XapianIndex::listDocuments(docIds, maxDocsCount, startDoc);
+}
+
+/// Lists documents that have a label.
+bool DBusXapianIndex::listDocumentsWithLabel(const string &name, set<unsigned int> &docIds,
+	unsigned int maxDocsCount, unsigned int startDoc) const
+{
+	return XapianIndex::listDocumentsWithLabel(name, docIds, maxDocsCount, startDoc);
+}
+
+//
+// Implementation of WritableIndexInterface
+//
+
+/// Indexes the given data.
+bool DBusXapianIndex::indexDocument(Tokenizer &tokens, const set<string> &labels,
+	unsigned int &docId)
+{
+	cerr << "DBusXapianIndex::indexDocument: not allowed" << endl;
+	return false;
+}
+
+/// Updates the given document; true if success.
+bool DBusXapianIndex::updateDocument(unsigned int docId, Tokenizer &tokens)
+{
+	bool updated = false;
+
+	DBusGConnection *pBus = getBusConnection();
+	if (pBus == NULL)
+	{
+		return false;
+	}
+
+	DBusGProxy *pBusProxy = getBusProxy(pBus);
+	if (pBusProxy == NULL)
+	{
+		cerr << "DBusXapianIndex::updateDocument: couldn't get bus proxy" << endl;
+		return false;
+	}
+
+	GError *pError = NULL;
+	if (dbus_g_proxy_call(pBusProxy, "UpdateDocument", &pError,
+		G_TYPE_UINT, docId,
+		G_TYPE_INVALID,
+		G_TYPE_UINT, &docId,
+		G_TYPE_INVALID) == TRUE)
+	{
+		updated = true;
+		reopen();
+	}
+	else
+	{
+		if (pError != NULL)
+		{
+			cerr << "DBusXapianIndex::updateDocument: " << pError->message << endl;
+			g_error_free(pError);
+		}
+	}
+
+	g_object_unref(pBusProxy);
+	// FIXME: don't we have to call dbus_g_connection_unref(pBus); ?
+
+	return updated;
+}
+
+/// Updates a document's properties.
+bool DBusXapianIndex::updateDocumentInfo(unsigned int docId, const DocumentInfo &docInfo)
+{
+	bool updated = false;
+
+	DBusGConnection *pBus = getBusConnection();
+	if (pBus == NULL)
+	{
+		return false;
+	}
+
+	DBusGProxy *pBusProxy = getBusProxy(pBus);
+	if (pBusProxy == NULL)
+	{
+		cerr << "DBusXapianIndex::updateDocumentInfo: couldn't get bus proxy" << endl;
+		return false;
+	}
+
+	GError *pError = NULL;
+	const char *pTitle = docInfo.getTitle().c_str();
+	const char *pLocation = docInfo.getLocation().c_str();
+	const char *pType = docInfo.getType().c_str();
+	const char *pLanguage = Languages::toEnglish(docInfo.getLanguage()).c_str();
+
+	if (dbus_g_proxy_call(pBusProxy, "SetDocumentInfo", &pError,
+		G_TYPE_UINT, docId,
+		G_TYPE_STRING, pTitle,
+		G_TYPE_STRING, pLocation,
+		G_TYPE_STRING, pType,
+		G_TYPE_STRING, pLanguage,
+		G_TYPE_INVALID,
+		G_TYPE_UINT, &docId,
+		G_TYPE_INVALID) == TRUE)
+	{
+		updated = true;
+		reopen();
+	}
+	else
+	{
+		if (pError != NULL)
+		{
+			cerr << "DBusXapianIndex::updateDocumentInfo: " << pError->message << endl;
+			g_error_free(pError);
+		}
+	}
+
+	g_object_unref(pBusProxy);
+	// FIXME: don't we have to call dbus_g_connection_unref(pBus); ?
+
+	return updated;
+}
+
+/// Sets a document's labels.
+bool DBusXapianIndex::setDocumentLabels(unsigned int docId, const set<string> &labels,
+	bool resetLabels)
+{
+	bool updatedLabels = false;
+
+	DBusGConnection *pBus = getBusConnection();
+	if (pBus == NULL)
+	{
+		return false;
+	}
+
+	DBusGProxy *pBusProxy = getBusProxy(pBus);
+	if (pBusProxy == NULL)
+	{
+		cerr << "DBusXapianIndex::setDocumentLabels: couldn't get bus proxy" << endl;
+		return false;
+	}
+
+	GError *pError = NULL;
+	dbus_uint32_t labelsCount = labels.size();
+	char *pLabels[labelsCount + 1];
+	unsigned int labelIndex = 0;
+
+	for (set<string>::const_iterator labelIter = labels.begin();
+		labelIter != labels.end(); ++labelIter)
+	{
+		pLabels[labelIndex] = g_strdup(labelIter->c_str());
+		++labelIndex;
+	}
+	pLabels[labelIndex] = NULL;
+
+	// G_TYPE_STRV is the GLib equivalent of DBUS_TYPE_ARRAY, DBUS_TYPE_STRING
+	if (dbus_g_proxy_call(pBusProxy, "SetDocumentLabels", &pError,
+		G_TYPE_UINT, docId,
+		G_TYPE_STRV, pLabels,
+		G_TYPE_BOOLEAN, resetLabels,
+		G_TYPE_INVALID,
+		G_TYPE_UINT, &docId,
+		G_TYPE_INVALID) == TRUE)
+	{
+		updatedLabels = true;
+		reopen();
+	}
+	else
+	{
+		if (pError != NULL)
+		{
+			cerr << "DBusXapianIndex::setDocumentLabels: " << pError->message << endl;
+			g_error_free(pError);
+		}
+	}
+
+	if (labelsCount > 0)
+	{
+		// Free the array
+		for (unsigned int labelNum = 0; labelNum < labelIndex; ++labelNum)
+		{
+			g_free(pLabels[labelNum]);
+		}
+	}
+
+	g_object_unref(pBusProxy);
+	// FIXME: don't we have to call dbus_g_connection_unref(pBus); ?
+
+	return updatedLabels;
+}
+
+/// Unindexes the given document; true if success.
+bool DBusXapianIndex::unindexDocument(unsigned int docId)
+{
+	cerr << "DBusXapianIndex::unindexDocument: not allowed" << endl;
+	return false;
+}
+
+/// Unindexes documents with the given label.
+bool DBusXapianIndex::unindexDocuments(const string &labelName)
+{
+	cerr << "DBusXapianIndex::unindexDocuments: not allowed" << endl;
+	return false;
+}
+
+/// Renames a label.
+bool DBusXapianIndex::renameLabel(const string &name, const string &newName)
+{
+	bool renamedLabel = false;
+
+	DBusGConnection *pBus = getBusConnection();
+	if (pBus == NULL)
+	{
+		return false;
+	}
+
+	DBusGProxy *pBusProxy = getBusProxy(pBus);
+	if (pBusProxy == NULL)
+	{
+		cerr << "DBusXapianIndex::renameLabel: couldn't get bus proxy" << endl;
+		return false;
+	}
+
+	GError *pError = NULL;
+	const char *pOldLabel = name.c_str();
+	const char *pNewLabel = newName.c_str();
+
+	if (dbus_g_proxy_call(pBusProxy, "RenameLabel", &pError,
+		G_TYPE_STRING, pOldLabel,
+		G_TYPE_STRING, pNewLabel,
+		G_TYPE_INVALID,
+		G_TYPE_STRING, &pNewLabel,
+		G_TYPE_INVALID) == TRUE)
+	{
+		renamedLabel = true;
+		reopen();
+	}
+	else
+	{
+		if (pError != NULL)
+		{
+			cerr << "DBusXapianIndex::renameLabel: " << pError->message << endl;
+			g_error_free(pError);
+		}
+	}
+
+	g_object_unref(pBusProxy);
+	// FIXME: don't we have to call dbus_g_connection_unref(pBus); ?
+
+	return renamedLabel;
+}
+
+/// Deletes all references to a label.
+bool DBusXapianIndex::deleteLabel(const string &name)
+{
+	bool deletedLabel = false;
+
+	DBusGConnection *pBus = getBusConnection();
+	if (pBus == NULL)
+	{
+		return false;
+	}
+
+	DBusGProxy *pBusProxy = getBusProxy(pBus);
+	if (pBusProxy == NULL)
+	{
+		cerr << "DBusXapianIndex::deleteLabel: couldn't get bus proxy" << endl;
+		return false;
+	}
+
+	GError *pError = NULL;
+	const char *pLabel = name.c_str();
+
+	if (dbus_g_proxy_call(pBusProxy, "DeleteLabel", &pError,
+		G_TYPE_STRING, pLabel,
+		G_TYPE_INVALID,
+		G_TYPE_STRING, &pLabel,
+		G_TYPE_INVALID) == TRUE)
+	{
+		deletedLabel = true;
+		reopen();
+	}
+	else
+	{
+		if (pError != NULL)
+		{
+			cerr << "DBusXapianIndex::deleteLabel: " << pError->message << endl;
+			g_error_free(pError);
+		}
+	}
+
+	g_object_unref(pBusProxy);
+	// FIXME: don't we have to call dbus_g_connection_unref(pBus); ?
+
+	return deletedLabel;
+}
+
+/// Flushes recent changes to the disk.
+bool DBusXapianIndex::flush(void)
+{
+	// There is no method for this because the daemon knows best when to flush
+	return true;
+}
---
+++ Index/DBusXapianIndex.h
@@ -0,0 +1,105 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _DBUS_XAPIAN_INDEX_H
+#define _DBUS_XAPIAN_INDEX_H
+
+#include <string>
+#include <set>
+
+#include "DocumentInfo.h"
+#include "XapianIndex.h"
+
+class DBusXapianIndex : public WritableIndexInterface, private XapianIndex
+{
+	public:
+		DBusXapianIndex(const std::string &indexName);
+		virtual ~DBusXapianIndex();
+
+		/// Returns false if the index couldn't be opened.
+		virtual bool isGood(void) const;
+
+		/// Gets the index location.
+		virtual std::string getLocation(void) const;
+
+		/// Returns a document's properties.
+		virtual bool getDocumentInfo(unsigned int docId, DocumentInfo &docInfo) const;
+
+		/// Determines whether a document has a label.
+		virtual bool hasLabel(unsigned int docId, const std::string &name) const;
+
+		/// Returns a document's labels.
+		virtual bool getDocumentLabels(unsigned int docId, std::set<std::string> &labels) const;
+
+		/// Checks whether the given URL is in the index.
+		virtual unsigned int hasDocument(const std::string &url) const;
+
+		/// Gets terms with the same root.
+		virtual unsigned int getCloseTerms(const std::string &term, std::set<std::string> &suggestions);
+
+		/// Returns the ID of the last document.
+		virtual unsigned int getLastDocumentID(void) const;
+
+		/// Returns the number of documents.
+		virtual unsigned int getDocumentsCount(const std::string &labelName = "") const;
+
+		/// Lists document IDs.
+		virtual unsigned int listDocuments(std::set<unsigned int> &docIds,
+			unsigned int maxDocsCount = 0, unsigned int startDoc = 0) const;
+
+		/// Lists documents that have a label.
+		virtual bool listDocumentsWithLabel(const std::string &name, std::set<unsigned int> &docIds,
+			unsigned int maxDocsCount = 0, unsigned int startDoc = 0) const;
+
+		/// Indexes the given data.
+		virtual bool indexDocument(Tokenizer &tokens, const std::set<std::string> &labels,
+			unsigned int &docId);
+
+		/// Updates the given document.
+		virtual bool updateDocument(unsigned int docId, Tokenizer &tokens);
+
+		/// Updates a document's properties.
+		virtual bool updateDocumentInfo(unsigned int docId, const DocumentInfo &docInfo);
+
+		/// Sets a document's labels.
+		virtual bool setDocumentLabels(unsigned int docId, const std::set<std::string> &labels,
+			bool resetLabels = true);
+
+		/// Unindexes the given document.
+		virtual bool unindexDocument(unsigned int docId);
+
+		/// Unindexes documents with the given label.
+		virtual bool unindexDocuments(const std::string &labelName);
+
+		/// Renames a label.
+		virtual bool renameLabel(const std::string &name, const std::string &newName);
+
+		/// Deletes all references to a label.
+		virtual bool deleteLabel(const std::string &name);
+
+		/// Flushes recent changes to the disk.
+		virtual bool flush(void);
+
+	protected:
+		void reopen(void);
+
+	private:
+		DBusXapianIndex(const DBusXapianIndex &other);
+		DBusXapianIndex &operator=(const DBusXapianIndex &other);
+
+};
+
+#endif // _DBUS_XAPIAN_INDEX_H
