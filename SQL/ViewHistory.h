9c7d6a9de40cfd39119a5b8df63008c9b294b86f
fabricecolin <fabricecolin@163c4839-fc77-d6c7-dbea-7b133176f809>
2006-10-24 05:48:19 +0000

    Added missing copyright notice.

--- SQL/ViewHistory.h
+++ SQL/ViewHistory.h
@@ -1,4 +1,6 @@
 /*
+ *  Copyright 2006,2006 Fabrice Colin
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
