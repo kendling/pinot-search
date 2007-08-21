8a98804734ac52b2a83c3da16d66656826a23b7c
fabricecolin <fabricecolin@163c4839-fc77-d6c7-dbea-7b133176f809>
2007-08-21 14:19:25 +0000

    List documents whose scheme is not file://.

--- scripts/bash/pinot-enum-index.sh
+++ scripts/bash/pinot-enum-index.sh
@@ -41,23 +41,36 @@ fi
 rm -f "$1/urls.txt" "$1/filesizes.txt"
 
 # Get a list of documents
-DOCIDS=`$DELVE -t X-MetaSE-Doc "$1"`
+DOCIDS=`$DELVE -t X-MetaSE-Doc "$1" | sed -e "s/[^0-9 ]//g"`
 if [ $? != 0 ]; then
   echo "Couldn't query database at $1"
 fi
 
 echo "Listing documents in index"
 echo "0" >> "$1/filesizes.txt"
-for DOCID in `echo $DOCIDS | sed -e "s/[^0-9 ]//g"` ;
+for DOCID in $DOCIDS ;
 do
   # Skip documents with a scheme other than file
   FILENAME=`$DELVE -d -r $DOCID "$1" | grep "url=file" | sed -e "s/url=\(.*\):\/\///g"`
+  #echo "File name is $FILENAME"
   if [ $? == 0 ] && [ ! -z "$FILENAME" ]; then
     FILESIZE=`du -b "$FILENAME" | sed -e "s/\([0-9]*\)\(.*\)/\1/g"`
     if [ ! -z "$FILESIZE" ]; then
-      echo $FILENAME >> "$1/urls.txt"
       echo $FILESIZE >> "$1/filesizes.txt"
       echo "+" >> "$1/filesizes.txt"
+    fi
+    echo $FILENAME >> "$1/urls.txt"
+  else
+    # Dump documents with a scheme other than file
+    URL=`$DELVE -d -r $DOCID "$1" | grep "url=" | sed -e "s/url=//g"`
+    #echo "URL is $URL"
+    if [ $? == 0 ] && [ ! -z "$URL" ]; then
+        FILESIZE=`$DELVE -d -r $DOCID "$1" | grep "size=" | sed -e "s/size=//g"`
+        if [ ! -z "$FILESIZE" ]; then
+          echo $FILESIZE >> "$1/filesizes.txt"
+          echo "+" >> "$1/filesizes.txt"
+        fi
+        echo $URL >> "$1/urls.txt"
     fi
   fi
 done
