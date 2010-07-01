79a336af9174cbbd44d8338db263aad2298c33ff
fabricecolin <fabricecolin@163c4839-fc77-d6c7-dbea-7b133176f809>
2010-07-01 10:26:40 +0000

    FreeBSD build patch by Thierry Thomas.

--- Core/DaemonState.cpp
+++ Core/DaemonState.cpp
@@ -37,7 +37,7 @@
       #define CHECK_DISK_SPACE 1
     #else
       #ifdef HAVE_SYS_MOUNT_H
-        #if defined(__OpenBSD__)
+        #if defined(__OpenBSD__) || (defined(__FreeBSD__) && (_FreeBSD_version < 700000))
           #include <sys/param.h>
         #endif
         #include <sys/mount.h>
